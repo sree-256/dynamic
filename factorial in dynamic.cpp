4cc41041fc0655f66e2e567de97de4f4605659d7
sree-256 <84057843+sree-256@users.noreply.github.com>
2021-06-07 19:56:30 +0530

    Add files via upload

---
+++ factorial in dynamic.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dp[1000005];
+int fact(int n)
+{
+	if(n<=1)
+		return 1;
+	if(dp[n]!=-1)
+	   return dp[n];
+	return dp[n]=n*fact(n-1);	
+}
+int main()
+{
+	int n;
+	cout<<"Enter a number to find the factorial :";
+	cin>>n;
+	for(int i=0;i<=n;i++)
+	{
+		dp[i]=-1;
+	}
+	cout<<fact(n);
+	return 0;
+}
